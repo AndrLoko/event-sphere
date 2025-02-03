a85480467f8207e8213e3204364652371a99a472
Andrew <andrewlokoffical@gmail.com>
2025-02-03 18:48:31 +0100

    Finished styling category selection

--- src/components/UI/category/category.jsx
+++ src/components/UI/category/category.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './category.scss'
 
-export default function category({ link }) {
+export default function category({ link, text }) {
   return (
     <>
-        <button className="category">
-            <img src={link} alt="" className="category__icon" />
-        </button>
+        <div className="category">
+            <button className="category">
+                <img src={link} alt="" className="category__icon" />
+            </button>
+            <p className='category__text'>{text}</p>
+        </div>
     </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,16 +7,18 @@ export default function Home() {
   return (
     <>
         <Slider />
-        <div className="container">
-            <div className="grid__categories">
-                <Category link="../../../public/SVG.png" />
-                <Category link="../../../public/SVG-1.png" />
-                <Category link="../../../public/SVG-2.png" />
-                <Category link="../../../public/SVG-3.png" />
-                <Category link="../../../public/SVG-4.png" />
-                <Category link="../../../public/SVG-5.png" />
-                <Category link="../../../public/SVG-6.png" />
-                <Category link="../../../public/SVG-7.png" />
+        <div className="categories__wrapper">
+            <div className="container">
+                <div className="grid__categories">
+                    <Category link="../../../public/SVG.png" text="Music"/>
+                    <Category link="../../../public/SVG-1.png" text="Nightlife"/>
+                    <Category link="../../../public/SVG-2.png" text="Performing & Visual Arts"/>
+                    <Category link="../../../public/SVG-3.png" text="Holidays"/>
+                    <Category link="../../../public/SVG-4.png" text="Dating"/>
+                    <Category link="../../../public/SVG-5.png" text="Hobbies"/>
+                    <Category link="../../../public/SVG-6.png" text="Business"/>
+                    <Category link="../../../public/SVG-7.png" text="Food & Drink"/>
+                </div>
             </div>
         </div>
     </>
